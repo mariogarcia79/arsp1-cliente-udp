a4c3fe3974041e34663b59c431ca8bd9fe43fad1
mariogarcia79 <74426959+mariogarcia79@users.noreply.github.com>
2025-10-25 20:30:43 +0000

    Fixing stuff

--- src/client.c
+++ src/client.c
@@ -8,30 +8,6 @@
 //#include <netinet/in.h> //double import from <arpa/inet.h>, not needed
 #include <netdb.h>
 
-#ifndef QOTD_H
-#define QOTD_H
-
-#define STRING_QOTD_MSG "Enviame el mensaje del dia"
-// Sizeof is resolved at compile time, effectively serving as strlen-like macro
-#define STRING_QOTD_MSG_LEN \
-    ( (sizeof(STRING_QOTD_MSG)/sizeof(STRING_QOTD_MSG[0])) - sizeof(STRING_QOTD_MSG[0]) )
-
-
-int qotd_setup_socket
-(
-    struct arguments   *args,
-    struct sockaddr_in *myaddr,
-    struct sockaddr_in *addr
-);
-
-int qotd_get_quote
-(
-    int sockfd,
-    struct sockaddr_in *addr,
-    char **received_msg
-);
-
-#endif /* QOTD_H */
 
 #ifndef ARGPARSE_H
 #define ARGPARSE_H
@@ -60,6 +36,32 @@ int parse_args
 );
 
 #endif /* ARGPARSE_H */
+
+#ifndef QOTD_H
+#define QOTD_H
+
+#define STRING_QOTD_MSG "Enviame el mensaje del dia"
+// Sizeof is resolved at compile time, effectively serving as strlen-like macro
+#define STRING_QOTD_MSG_LEN \
+    ( (sizeof(STRING_QOTD_MSG)/sizeof(STRING_QOTD_MSG[0])) - sizeof(STRING_QOTD_MSG[0]) )
+
+
+int qotd_setup_socket
+(
+    struct arguments   *args,
+    struct sockaddr_in *myaddr,
+    struct sockaddr_in *addr
+);
+
+int qotd_get_quote
+(
+    int sockfd,
+    struct sockaddr_in *addr,
+    char **received_msg
+);
+
+#endif /* QOTD_H */
+
 
 #ifndef CONFIG_H
 #define CONFIG_H
